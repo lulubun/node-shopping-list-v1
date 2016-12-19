5a89a1f665222d487264054ae13a81803a6016e0
lulubun <laurapetitt@gmail.com>
2016-12-18 23:08:43 -0500

    updated server file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,16 +12,24 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
+// we're going to add some items to Recipes
 // so there's some data to look at
 ShoppingList.create('beans', true);
 ShoppingList.create('tomatoes', false);
 ShoppingList.create('peppers', false);
 
+Recipes.create('tea', ['tea', 'hot water']);
+Recipes.create('Omlet du Fromage', ['eggs', 'cheese']);
+Recipes.create('hot dog', ['hot dog', 'bun', 'ketchup'] );
+
 // when the root of this router is called with GET, return
-// all current ShoppingList items
+// all current Recipes items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
